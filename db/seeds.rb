885bfde69073b7c3fabcedd3155880440ecb9915
nickmjones <hello@nickmjones.me>
2018-10-03 15:26:12 -0400

    Sets up seed file to add authors and nothing else.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'faker'
-
 p "Creating authors..."
 Author.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('authors')
@@ -21,18 +19,3 @@ Author.create(
     full_name: 'Nick Jones',
     shortbio: 'Event Farm person'
 )
-
-p "Creating posts..."
-Post.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!('posts')
-5.times do
-  Post.create(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph(12, false, 8),
-    meta_desc: Faker::Lorem.sentence,
-    meta_keys: 'foo, bar, bat, baz',
-    created_at: DateTime.now,
-    author_id: 1
-    )  
-end
-p "Done."
