04f2fb7e27dda46a114809204923e35613a3b7aa
nickmjones <hello@nickmjones.me>
2018-10-03 20:34:38 -0400

    Variety of UI fixes - Fixes for sharing with open graph - Responsive tweaks to make things look better on mobile

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,13 @@
+module MetaTagsHelper
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
+  end
+
+  def meta_keywords
+    content_for?(:meta_keywords) ? content_for(:meta_keywords) : DEFAULT_META["meta_keywords"]
+  end
+end
